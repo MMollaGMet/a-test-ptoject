abcdc6e00cd60ad28a34053889c9c76582fe5ee2
Michael Molla <47782408+MMollaGMet@users.noreply.github.com>
2019-02-19 08:42:35 -0500

    added some code

---
+++ hello_world.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main(int argc, char** argv)
+{
+  int i;
+
+  printf("Hello world!\n");fflush(NULL);
+
+  for(i=0;i<argc;i++)
+  {
+    printf("[%s]\n",argv[i]);
+  }
+
+}
